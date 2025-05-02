3e3da66a546ec001c54e62ec0b0b8db51a045226
Jan Egil Stafff Øvretveit <janstovr@gmail.com>
2025-05-02 15:51:02 +0200

    NextAuth Sign in

--- components/TopNav.js
+++ components/TopNav.js
@@ -1,19 +1,40 @@
+"use client";
+
 import Link from "next/link";
+import { useSession, signOut } from "next-auth/react";
 
 const TopNav = () => {
+  const { data, status, loading } = useSession();
+
   return (
     <nav className="nav shadow p-2 justify-content-between mb-3">
       <Link href="/" className="nav-link">
         🛒 NEXTECOM
       </Link>
-      <div className="d-flex">
-        <Link href="/login" className="nav-link">
-          Login
-        </Link>
-        <Link href="/register" className="nav-link">
-          Register
-        </Link>
-      </div>
+
+      {status === "authenticated" ? (
+        <>
+          <Link href="/dashboard/user" className="nav-link">
+            {data?.user?.name}
+          </Link>
+          <a
+            href="/logout"
+            className="nav-link"
+            onClick={() => signOut({ callbackUrl: "/login" })}
+          >
+            Logout
+          </a>
+        </>
+      ) : (
+        <div className="d-flex">
+          <Link href="/login" className="nav-link">
+            Login
+          </Link>
+          <Link href="/register" className="nav-link">
+            Register
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ import config from "./config.js";
 const nextConfig = {
   env: {
     DB_URI: config.DB_URI,
-    API: config.API
+    API: config.API,
+    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
   },
 };
 
